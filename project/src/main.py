55aedc1627ef6f7c06fece0a66ecbc675c8a34ab
harshkk1911 <129595349+harshkk1911@users.noreply.github.com>
2026-01-06 23:48:48 +0530

    Add main function for user input and error handling

--- project/src/main.py
+++ project/src/main.py
@@ -1,3 +1,13 @@
 def sum_of_2_numbers(num1,num2):
   print('SUM OF ',num1,' and ',num2,' is ',num1+num2)
-sum_of_2_numbers(3,4)
+
+def main():
+  try:
+    num1=int(input('Enter number 1:'))
+    num2=int(input('Enter number 2:'))
+    sum_of_2_numbers(num1,num2)
+  except ValueError:
+    print('INVALID INPUT')
+
+if __name__=="__main__":
+  main()
